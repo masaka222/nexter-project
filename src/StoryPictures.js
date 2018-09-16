2e505e8b76564023ce595922bd721415250928a7
Vasilis Georgoudis <masakaftw@hotmail.com>
2018-09-16 00:01:40 -0400

    Story-photos complete

--- src/StoryPictures.js
+++ src/StoryPictures.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
 import './StoryPictures.css';
+import couple from "./img/story-1.jpeg";
+import house from "./img/story-2.jpeg";
 
 class StoryPictures extends Component {
     render() {
       return (
         <div className="story__pictures">
-            Story pictures
+            <img src={couple} alt="Couple with new house" className="story__img--1"/>
+            <img src={house} alt="New house" className="story__img--2"/>
         </div>
       );
     }
