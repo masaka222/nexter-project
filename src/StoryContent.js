9ba634ca8bbc92402920ada50e3bc26cc3d5439c
Vasilis Georgoudis <masakaftw@hotmail.com>
2018-09-15 22:26:23 -0400

    Story-content component finished

--- src/StoryContent.js
+++ src/StoryContent.js
@@ -5,7 +5,14 @@ class StoryContent extends Component {
     render() {
       return (
         <div className="story__content">
-            Story content
+            <h3 className="heading-3 mb-sm">Happy Customers</h3>
+            <h2 className="heading-2--dark mb-md">&ldquo;The best decision of our lives&rdquo;</h2>
+            <p className="story__text mb-lg">
+              Lorem ipsum dolor sit amet consectetur adipiscing elit, molestie pharetra 
+              maecenas est curabitur odio laoreet, non facilisi suspendisse duis luctus eu. 
+              Ultrices cum posuere tellus lobortis sapien nibh pulvinar.
+            </p>
+            <button className="btn">Find your own home</button>
         </div>
       );
     }
