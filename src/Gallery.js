e47fcd96e88e7cf9595b96aa4d969b6c49ec33a0
Vasilis Georgoudis <masakaftw@hotmail.com>
2018-09-22 23:39:20 -0400

    Gallery Grid items placed

--- src/Gallery.js
+++ src/Gallery.js
@@ -20,10 +20,48 @@ class Gallery extends Component {
     render() {
       return (
         <section className="gallery">
-        <figure className="gallery__item gallery__item--1">
-          <img src={gal1} alt="Gallery image 1" className="gallery__img"></img>
-        </figure>
-      
+          <figure className="gallery__item gallery__item--1">
+            <img src={gal1} alt="Gallery image 1" className="gallery__img"></img>
+          </figure>
+          <figure className="gallery__item gallery__item--2">
+            <img src={gal2} alt="Gallery image 2" className="gallery__img"></img>
+          </figure>
+          <figure className="gallery__item gallery__item--3">
+            <img src={gal3} alt="Gallery image 3" className="gallery__img"></img>
+          </figure>
+          <figure className="gallery__item gallery__item--4">
+            <img src={gal4} alt="Gallery image 4" className="gallery__img"></img>
+          </figure>
+          <figure className="gallery__item gallery__item--5">
+            <img src={gal5} alt="Gallery image 5" className="gallery__img"></img>
+          </figure>
+          <figure className="gallery__item gallery__item--6">
+            <img src={gal6} alt="Gallery image 6" className="gallery__img"></img>
+          </figure>
+          <figure className="gallery__item gallery__item--7">
+            <img src={gal7} alt="Gallery image 7" className="gallery__img"></img>
+          </figure>
+          <figure className="gallery__item gallery__item--8">
+            <img src={gal8} alt="Gallery image 8" className="gallery__img"></img>
+          </figure>
+          <figure className="gallery__item gallery__item--9">
+            <img src={gal9} alt="Gallery image 9" className="gallery__img"></img>
+          </figure>
+          <figure className="gallery__item gallery__item--10">
+            <img src={gal10} alt="Gallery image 10" className="gallery__img"></img>
+          </figure>
+          <figure className="gallery__item gallery__item--11">
+            <img src={gal11} alt="Gallery image 11" className="gallery__img"></img>
+          </figure>
+          <figure className="gallery__item gallery__item--12">
+            <img src={gal12} alt="Gallery image 12" className="gallery__img"></img>
+          </figure>
+          <figure className="gallery__item gallery__item--13">
+            <img src={gal13} alt="Gallery image 13" className="gallery__img"></img>
+          </figure>
+          <figure className="gallery__item gallery__item--14">
+            <img src={gal14} alt="Gallery image 14" className="gallery__img"></img>
+          </figure>
         </section>
       );
     }
