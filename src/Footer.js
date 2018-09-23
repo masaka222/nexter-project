93570295d9b4a34c77543cb1b6a550b0a5103d90
Vasilis Georgoudis <masakaftw@hotmail.com>
2018-09-23 00:59:22 -0400

    Footer done

--- src/Footer.js
+++ src/Footer.js
@@ -5,7 +5,17 @@ class Footer extends Component {
     render() {
       return (
         <footer className="footer">
-            Footer
+            <nav>
+              <li className="nav__item"><a href="#" className="nav__link">Find your dream home</a></li>
+              <li className="nav__item"><a href="#" className="nav__link">Request proposal</a></li>
+              <li className="nav__item"><a href="#" className="nav__link">Download home planner</a></li>
+              <li className="nav__item"><a href="#" className="nav__link">Contact us</a></li>
+              <li className="nav__item"><a href="#" className="nav__link">Submit your property</a></li>
+              <li className="nav__item"><a href="#" className="nav__link">Come work with us</a></li>
+            </nav>
+            <p className="copyright">
+              &copy; Copyright 2018 Nexter Project
+            </p>
         </footer>
       );
     }
