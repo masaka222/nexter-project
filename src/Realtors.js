238ffb1e534df96e5da5c5b7eb137636eb24c02b
Vasilis Georgoudis <masakaftw@hotmail.com>
2018-09-28 00:43:20 -0400

    Header finished

--- src/Realtors.js
+++ src/Realtors.js
@@ -1,11 +1,33 @@
 import React, { Component } from 'react';
 import './Realtors.css';
+import realtor1 from "./img/realtor-1.jpeg";
+import realtor2 from "./img/realtor-2.jpeg";
+import realtor3 from "./img/realtor-3.jpeg";
 
 class Realtors extends Component {
     render() {
       return (
         <div className="realtors">
-            Top 3 Realtors
+            <h3 className="heading-3">Top 3 Realtors</h3>
+            <div className="realtors__list">
+              <img src={realtor1} alt="Realtor 1" className="realtors__img"/>
+              <div className="realtors__details">
+                <h4 className="heading-4 heading-4--light"> Erik Feinman</h4>
+                <p className="realtors__sold">245 houses sold</p>
+              </div>
+
+              <img src={realtor2} alt="Realtor 2" className="realtors__img"/>
+              <div className="realtors__details">
+                <h4 className="heading-4 heading-4--light"> Kim Brown</h4>
+                <p className="realtors__sold">212 houses sold</p>
+              </div>
+
+              <img src={realtor3} alt="Realtor 3" className="realtors__img"/>
+              <div className="realtors__details">
+                <h4 className="heading-4 heading-4--light"> Toby Ramsey</h4>
+                <p className="realtors__sold">198 houses sold</p>
+              </div>
+            </div>
         </div>
       );
     }
